76c176a71b2565d419f4e4cd1db830d1290f0a5e
Andrey Aleksandrov <andrey.aleksandrov@octo3.fi>
2019-07-31 16:00:30 +0300

    Add magit-todos package, keybind global-display-line-numbers-mode

--- custom.el
+++ custom.el
@@ -33,7 +33,7 @@
      ("???" . "#dc752f"))))
  '(package-selected-packages
    (quote
-    (keyfreq dashboard calfw latex-preview-pane flymd markdown-mode+ ox-twbs org-bullets htmlize rainbow-mode docker company-lsp company editorconfig multi-term yasnippet-snippets elogcat flycheck-kotlin flycheck-popup-tip dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode lsp-java lsp-ui lsp-mode anzu swiper-helm evil-nerd-commenter general elscreen helm-projectile projectile magit exec-path-from-shell which-key helm-ag helm treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer highlight-parentheses delight spacemacs-theme key-chord evil-surround use-package)))
+    (magit-todos keyfreq dashboard calfw latex-preview-pane flymd markdown-mode+ ox-twbs org-bullets htmlize rainbow-mode docker company-lsp company editorconfig multi-term yasnippet-snippets elogcat flycheck-kotlin flycheck-popup-tip dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode lsp-java lsp-ui lsp-mode anzu swiper-helm evil-nerd-commenter general elscreen helm-projectile projectile magit exec-path-from-shell which-key helm-ag helm treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer highlight-parentheses delight spacemacs-theme key-chord evil-surround use-package)))
  '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef")))
  '(projectile-globally-ignored-directories
    (quote
