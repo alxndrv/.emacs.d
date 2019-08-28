95e7a4d1f4299760c3f440e5b351d3785875ffed
Andrey Aleksandrov <andrey.aleksandrov@visma.com>
2019-08-28 16:03:51 +0300

    Add helm-lsp

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#d2ceda" "#f2241f" "#67b11d" "#b1951d" "#3a81c3" "#a31db1" "#21b8c7" "#655370"])
+ '(bongocat-mode t)
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "ecba61c2239fbef776a72b65295b88e5534e458dfe3e6d7d9f9cb353448a569e" "3d5720f488f2ed54dd4e40e9252da2912110948366a16aef503f3e9e7dfe4915" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
@@ -38,7 +39,7 @@
      ("???" . "#dc752f"))))
  '(package-selected-packages
    (quote
-    (powerline magit-todos keyfreq dashboard calfw latex-preview-pane flymd markdown-mode+ ox-twbs org-bullets htmlize rainbow-mode docker company-lsp company editorconfig multi-term yasnippet-snippets elogcat flycheck-kotlin flycheck-popup-tip dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode lsp-java lsp-ui lsp-mode anzu swiper-helm evil-nerd-commenter general elscreen helm-projectile projectile magit exec-path-from-shell which-key helm-ag helm treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer highlight-parentheses delight spacemacs-theme key-chord evil-surround use-package)))
+    (helm-lsp powerline magit-todos keyfreq dashboard calfw latex-preview-pane flymd markdown-mode+ ox-twbs org-bullets htmlize rainbow-mode docker company-lsp company editorconfig multi-term yasnippet-snippets elogcat flycheck-kotlin flycheck-popup-tip dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode lsp-java lsp-ui lsp-mode anzu swiper-helm evil-nerd-commenter general elscreen helm-projectile projectile magit exec-path-from-shell which-key helm-ag helm treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer highlight-parentheses delight spacemacs-theme key-chord evil-surround use-package)))
  '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef")))
  '(projectile-globally-ignored-directories
    (quote
