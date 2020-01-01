b779b3753bdb6b298ff9b41b984c3dc1f0bc6f1b
Andrey Aleksandrov <andrey.aleksandrov0@gmail.com>
2020-01-01 16:19:46 +0200

    Rewrite configuration for most basic settings

--- custom.el
+++ custom.el
@@ -3,50 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#d2ceda" "#f2241f" "#67b11d" "#b1951d" "#3a81c3" "#a31db1" "#21b8c7" "#655370"])
- '(bongocat-mode t)
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "ecba61c2239fbef776a72b65295b88e5534e458dfe3e6d7d9f9cb353448a569e" "3d5720f488f2ed54dd4e40e9252da2912110948366a16aef503f3e9e7dfe4915" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(editorconfig-mode t)
- '(global-auto-revert-mode t)
- '(global-company-mode t)
- '(global-diff-hl-mode nil)
- '(global-display-line-numbers-mode nil)
- '(global-eldoc-mode nil)
- '(global-linum-mode nil)
- '(global-magit-file-mode nil)
- '(global-whitespace-mode nil)
- '(helm-ff-file-name-history-use-recentf t)
- '(helm-ff-lynx-style-map t)
- '(helm-ff-skip-boring-files t)
- '(hl-todo-keyword-faces
-   (quote
-    (("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#2d9574")
-     ("PROG" . "#3a81c3")
-     ("OKAY" . "#3a81c3")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#42ae2c")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#b1951d")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX" . "#dc752f")
-     ("XXXX" . "#dc752f")
-     ("???" . "#dc752f"))))
  '(package-selected-packages
    (quote
-    (elfeed-org elfeed helm-lsp powerline magit-todos keyfreq dashboard calfw latex-preview-pane flymd markdown-mode+ ox-twbs org-bullets htmlize rainbow-mode docker company-lsp company editorconfig multi-term yasnippet-snippets elogcat flycheck-kotlin flycheck-popup-tip dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode lsp-java lsp-ui anzu swiper-helm evil-nerd-commenter general elscreen helm-projectile projectile magit exec-path-from-shell which-key helm-ag helm treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer highlight-parentheses delight spacemacs-theme key-chord evil-surround use-package)))
- '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef")))
- '(projectile-globally-ignored-directories
-   (quote
-    (".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "build")))
- '(size-indication-mode nil))
+    (keyfreq dashboard calfw latex-preview-pane flymd markdown-mode+ ox-twbs org-bullets htmlize elfeed-org elfeed rainbow-mode docker company-lsp company editorconfig multi-term yasnippet-snippets elogcat flycheck-kotlin flycheck-popup-tip dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode lsp-java lsp-ui helm-lsp lsp-mode anzu swiper-helm evil-nerd-commenter general elscreen helm-projectile projectile magit-todos magit exec-path-from-shell which-key helm-ag helm treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer highlight-parentheses powerline delight spacemacs-theme key-chord evil-surround use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,3 +1,19 @@
 
+;; Bootstrap package management
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Setting up the package manager. Install if missing.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t))
+
 (require 'org)
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
