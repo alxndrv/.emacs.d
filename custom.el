9269f047d0c48cf25a7aa72085ca1430e29fb0a9
Andrey Aleksandrov <andrey.aleksandrov0@gmail.com>
2020-01-01 18:44:59 +0200

    Make ivy prompts prettier

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (diminish keyfreq latex-preview-pane markdown-mode+ ox-twbs htmlize org-bullets elfeed-org elfeed docker editorconfig multi-term yasnippet-snippets elogcat dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode flycheck-kotlin flycheck-popup-tip flycheck company-lsp company lsp-java lsp-ui lsp-ivy lsp-mode anzu evil-nerd-commenter general elscreen magit-todos magit exec-path-from-shell which-key ivy-posframe counsel-projectile counsel dashboard treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer powerline spacemacs-theme key-chord evil-surround evil use-package))))
+    (all-the-icons-ivy all-the-icons-color-icons flx ivy-rich yasnippet-snippets yaml-mode which-key web-mode use-package treemacs-icons-dired tide swift-mode spacemacs-theme rjsx-mode powerline php-mode ox-twbs org-bullets omnisharp multi-term markdown-mode+ magit-todos lsp-ui lsp-java lsp-ivy latex-preview-pane kotlin-mode keyfreq key-chord ivy-posframe htmlize highlight-indent-guides groovy-mode general flycheck-popup-tip flycheck-kotlin exec-path-from-shell evil-surround evil-nerd-commenter elscreen elogcat elfeed-org editorconfig dumb-jump dockerfile-mode docker dimmer diminish dashboard counsel-projectile company-lsp beacon anzu android-mode all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
