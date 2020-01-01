acaf90a676e672a8bfc2e4561c6dc5561503e629
Andrey Aleksandrov <andrey.aleksandrov0@gmail.com>
2020-01-01 18:09:29 +0200

    Switch to using Ivy instead of Helm

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (diminish keyfreq latex-preview-pane markdown-mode+ ox-twbs htmlize org-bullets elfeed-org elfeed docker editorconfig multi-term yasnippet-snippets elogcat dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode flycheck-kotlin flycheck-popup-tip flycheck company-lsp company lsp-java lsp-ui helm-lsp lsp-mode anzu swiper-helm evil-nerd-commenter general elscreen helm-projectile projectile magit-todos magit exec-path-from-shell which-key helm-ag helm dashboard treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer powerline spacemacs-theme key-chord evil-surround evil use-package))))
+    (diminish keyfreq latex-preview-pane markdown-mode+ ox-twbs htmlize org-bullets elfeed-org elfeed docker editorconfig multi-term yasnippet-snippets elogcat dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode flycheck-kotlin flycheck-popup-tip flycheck company-lsp company lsp-java lsp-ui lsp-ivy lsp-mode anzu evil-nerd-commenter general elscreen magit-todos magit exec-path-from-shell which-key ivy-posframe counsel-projectile counsel dashboard treemacs-icons-dired highlight-indent-guides all-the-icons beacon dimmer powerline spacemacs-theme key-chord evil-surround evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
