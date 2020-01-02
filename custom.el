cca6ead88eb29bee55bbaafdbf31783feaeb98ac
Andrey Aleksandrov <andrey.aleksandrov@visma.com>
2020-01-02 10:18:43 +0200

    Configure `Prettier` integration

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (all-the-icons-ivy all-the-icons-color-icons flx ivy-rich yasnippet-snippets yaml-mode which-key web-mode use-package treemacs-icons-dired tide swift-mode spacemacs-theme rjsx-mode powerline php-mode ox-twbs org-bullets omnisharp multi-term markdown-mode+ magit-todos lsp-ui lsp-java lsp-ivy latex-preview-pane kotlin-mode keyfreq key-chord ivy-posframe htmlize highlight-indent-guides groovy-mode general flycheck-popup-tip flycheck-kotlin exec-path-from-shell evil-surround evil-nerd-commenter elscreen elogcat elfeed-org editorconfig dumb-jump dockerfile-mode docker dimmer diminish dashboard counsel-projectile company-lsp beacon anzu android-mode all-the-icons))))
+    (prettier-js add-node-modules-path yasnippet-snippets yaml-mode which-key web-mode use-package treemacs-icons-dired tide swift-mode spacemacs-theme rjsx-mode powerline php-mode ox-twbs org-bullets omnisharp multi-term markdown-mode+ magit-todos lsp-ui lsp-java lsp-ivy latex-preview-pane kotlin-mode keyfreq key-chord ivy-rich ivy-posframe htmlize highlight-indent-guides groovy-mode general flycheck-popup-tip flycheck-kotlin flx exec-path-from-shell evil-surround evil-nerd-commenter elscreen elogcat elfeed-org editorconfig dumb-jump dockerfile-mode docker dimmer diminish dashboard counsel-projectile company-lsp beacon anzu android-mode all-the-icons-ivy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
