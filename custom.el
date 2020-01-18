904aa4cfea46a162e60bc64ef2da7ae196a6f14f
Andrey Aleksandrov <andrey.aleksandrov0@gmail.com>
2020-01-18 12:58:01 +0200

    Change theme to doom-dracula

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ivy-xref diminish keyfreq latex-preview-pane markdown-mode+ ox-twbs htmlize org-bullets elfeed-org elfeed prettier-js add-node-modules-path docker editorconfig multi-term yasnippet-snippets elogcat dumb-jump dockerfile-mode yaml-mode php-mode swift-mode groovy-mode android-mode kotlin-mode omnisharp json-mode web-mode tide rjsx-mode flycheck-kotlin flycheck-popup-tip flycheck company-lsp company lsp-java lsp-ui lsp-ivy lsp-mode anzu evil-nerd-commenter general elscreen magit-todos magit exec-path-from-shell which-key ivy-posframe ivy-rich flx counsel-projectile counsel dashboard treemacs-icons-dired all-the-icons-ivy all-the-icons highlight-indent-guides beacon solaire-mode powerline spacemacs-theme key-chord evil-surround evil use-package))))
+    (doom-themes yasnippet-snippets yaml-mode which-key web-mode use-package typescript-mode treemacs-icons-dired swift-mode spacemacs-theme solaire-mode rjsx-mode prettier-js powerline php-mode ox-twbs org-bullets omnisharp multi-term markdown-mode+ magit-todos lsp-ui lsp-java lsp-ivy latex-preview-pane kotlin-mode keyfreq key-chord ivy-xref ivy-rich ivy-posframe htmlize highlight-indent-guides groovy-mode general flycheck-popup-tip flycheck-kotlin flx exec-path-from-shell evil-surround evil-nerd-commenter elscreen elogcat elfeed-org editorconfig dumb-jump dockerfile-mode docker diminish dashboard counsel-projectile company-lsp beacon anzu android-mode all-the-icons-ivy add-node-modules-path))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
